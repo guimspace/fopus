9c3df95d57a79dfd179ac6e5c50df60ef31c28cc
Guilherme T Maeoka <gui.mspace@gmail.com>
2019-03-30 01:16:31 -0300

    Bump version to alpha-0.2.0
    - Warn when backup file already exists
    - Minimize and simplify echo of progress
    - Show details of backup file
    - Fixes #1

--- src/fopus.sh
+++ src/fopus.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-version=alpha-0.1.0
+version=alpha-0.2.0
 
 # https://unix.stackexchange.com/a/232083 for the future
 # unset -v password
