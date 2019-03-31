dbc5707b02f0156e9aca365ff710050c730457d9
Guilherme T Maeoka <gui.mspace@gmail.com>
2019-03-31 11:01:11 -0300

    Bump version to 0.3.0 and fix versioning

--- src/fopus.sh
+++ src/fopus.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-version=alpha-0.2.0
+version=0.3.0
 
 # https://unix.stackexchange.com/a/232083 for the future
 # unset -v password
