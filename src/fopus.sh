ea4baa0e7a0d3b52b0b06062a3730c5dc7283fda
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-12-10 10:46:45 -0300

    Miscellaneous revamp

--- src/fopus.sh
+++ src/fopus.sh
@@ -24,36 +24,35 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-set +o allexport
-version=1.6.1
-
 if [[ "$UID" -lt 1000 ]]; then
 	exit 1
 fi
 
-typeset -A fopus_config
-fopus_config=(
-    [max-size]="1073741824"
-	[root-path]="$HOME/Backups/"
+declare -r VERSION="1.6.1"
+
+DATE=$(date +%Y-%m-%d)
+declare -r DATE
+
+DRY_RUN="false"
+
+declare -A CONFIG=(
+	[part-size]=""
+	[repo-path]="$HOME/Backups/"
 	[group-by-name]="false"
 	[one]="false"
 )
 
-DATE=$(date +%Y-%m-%d)
-DRY_RUN=false
-
 check_requirements()
 {
-	local list_packages=(age tar xz md5sum shasum)
-	local i=""
+	declare -ar apps=(age tar xz md5sum shasum)
+	local app=""
 
-	for i in ${!list_packages[*]}; do
-		if ! command -v "${list_packages[$i]}" &> /dev/null; then
-			>&2 echo "fopus: ${list_packages[$i]} not found"
+	for app in "${apps[@]}"; do
+		if ! command -v "${app}" &> /dev/null; then
+			>&2 echo "fopus: ${app} not found"
 			exit 1
 		fi
 	done
-
 
   	sha256sum_func
     if [[ -z "$sha256sum_tool" ]]; then
@@ -88,369 +87,321 @@ sha1sum_func() {
 
 show_help()
 {
-	echo "Usage: fopus [OPTION]... [FILE]..."
-	echo "Archive, compress, encrypt and split (aces) the FILE(s)."
+	echo "Usage: fopus [OPTION]... FILE..."
 	echo ""
 	echo "Commands:"
-	echo ""
-	echo -e "  --help\t\tdisplay this short help"
-	echo -e "  --version\t\tdisplay the version number"
+	echo -e "\t--help\t\tdisplay this short help"
+	echo -e "\t--version\tdisplay version number"
 	echo ""
 	echo "Options:"
-	echo ""
-	echo -e "  --no-split\t\tskip split process"
-	echo -e "  --one\t\tall-in-one"
-	echo -e "  --group-by-name\t\tgroup backups by file/date instead of date/name"
-	echo ""
-	echo "To aces a file whose name starts with a '-', for example '-foo',"
-	echo "use one of these commands:"
-	echo "  fopus -- -foo"
-	echo ""
-	echo "  fopus ./-foo"
-	echo ""
-	echo "Report bugs, comments and suggestions to <gui.mspace@gmail.com> (in English or Portuguese)."
-	echo "fopus repository: <https://github.com/guimspace/fopus>"
+	echo -e "\t--one\t\tput FILEs in one backup."
+	echo -e "\t--no-split\tdon't split backup in chunks."
+	echo -e "\t--group-by-name\tgroup backups by file/date instead of date/name."
+	echo -e "\t--output OUTPUT\tbackup in the directory at path OUTPUT."
 }
 
-fopus_main()
+main()
 {
-	local list_args=("$@")
+	declare -r ORIGIN="$(pwd -P)"
+	declare -a FILES=()
 
-	local i=""
-	local N=""
-	declare -a list_files
-	declare -a list_clean
-
-	origins_path="$(pwd -P)"
-
-	if [[ ${#list_args[@]} -eq 0 ]]; then
+	if [[ -z "$*" ]]; then
 		>&2 echo "fopus: missing file operand"
 		echo "Try 'fopus --help' for more information."
 		exit 1
 	fi
 
-	evaluate_options "${list_args[@]}"
-	filter_evaluate_files
+	evaluate_arguments "$@"
+	declare -r DRY_RUN="$DRY_RUN"
 
-	root_path="${fopus_config[root-path]}"
+	OUTPUT_PATH="${CONFIG[repo-path]}"
 
-	if [[ "$root_path" =~ ^"$HOME"/?$ ]]; then
-		root_path="$HOME/Backups"
+	if [[ ! -d "$OUTPUT_PATH" ]]; then
+		>&2 echo "fopus: $OUTPUT_PATH: No such directory"
+		exit 1
+	elif [[ "$OUTPUT_PATH" =~ ^"$HOME"/?$ ]]; then
+		>&2 echo "fopus: $OUTPUT_PATH: Permission denied"
+		exit 1
+	elif [[ ! -w "$OUTPUT_PATH" ]]; then
+		>&2 echo "fopus: $OUTPUT_PATH: Permission denied"
+		exit 1
 	fi
 
-	if [[ ! -d "$root_path" ]]; then
-		>&2 echo "fopus: $root_path: No such directory"
-		exit 1
-	elif [[ ! -w "$root_path" ]]; then
-		>&2 echo "fopus: $root_path: Permission denied"
-		exit 1
-	fi
+	OUTPUT_PATH=${OUTPUT_PATH%/}
+	declare -r OUTPUT_PATH="$OUTPUT_PATH"
 
-	root_path=${root_path%/}
+	evaluate_files
 
-	if [[ "${fopus_config[one]}" == "true" ]]; then
+	if [[ "${CONFIG[one]}" = "true" ]]; then
 		echo ""
-		fopus_backup_main "${list_clean[@]}"
+		fopus_backup "${FILES[@]}"
 	else
-		i=1
-		N="${#list_clean[@]}"
-		while [[ $i -le $N ]]; do
+		declare -i i=0
+		declare -ir N="${#FILES[@]}"
+		local file=""
+		for file in "${FILES[@]}"; do
+			((i++))
 			echo ""
-			echo "fopus: ${list_clean[$i-1]} ($i/$N)"
-			fopus_backup_main "${list_clean[$i-1]}"
-
-			i=$((i+1))
+			echo "fopus: ${FILES[$i-1]} ($i of $N)"
+			fopus_backup "$file"
 		done
 	fi
 
 	exit 0
 }
 
-evaluate_options()
+evaluate_arguments()
 {
-	local list_args=("$@")
+	declare -ar ARGS=("$@")
 
-	local i=0
-	local N=${#list_args[@]}
+	local -ir N=${#ARGS[@]}
+	local -i i=0
 
-	while [[ $i -lt $N && "${list_args[$i]}" != "--" ]]; do
-		case "${list_args[$i]}" in
-			--dry-run)
-				DRY_RUN=true ;;
+	while [[ $i -lt $N && "${ARGS[$i]}" != "--" ]]; do
+		case "${ARGS[$i]}" in
+			"--dry-run")
+				DRY_RUN="true" ;;
 
-			--group-by-name)
-				fopus_config[group-by-name]="true" ;;
+			"--group-by-name")
+				CONFIG[group-by-name]="true" ;;
 
-			--one)
-				fopus_config[one]="true" ;;
+			"--one")
+				CONFIG[one]="true" ;;
 
-			--no-split)
-				fopus_config[max-size]="-1" ;;
-
-			--output)
-				i=$((i+1))
-				if [[ ! -d "${list_args[$i]}" ]]; then
-					>&2 echo "fopus: ${list_args["$i"]}: invalid argument"
+			"--no-split")
+				if [[ -n "${CONFIG[part-size]}" ]]; then
+					>&2 echo "fopus: --split-size can't be used with --no-split"
 					exit 1
 				fi
-				fopus_config[root-path]="${list_args[$i]}" ;;
+				CONFIG[part-size]="-1" ;;
 
-			--)
+			"--split-size")
+				if [[ -n "${CONFIG[part-size]}" ]]; then
+					>&2 echo "fopus: --split-size can't be used with --no-split"
+					exit 1
+				fi
+				((i++))
+				if ! split --bytes="${ARGS["$i"]}" /dev/null; then
+					exit 1
+				fi
+				CONFIG[part-size]="${ARGS["$i"]}" ;;
+
+			"--output")
+				((i++))
+				if [[ ! -d "${ARGS[$i]}" ]]; then
+					>&2 echo "fopus: ${ARGS["$i"]}: No such directory"
+					exit 1
+				fi
+				CONFIG[repo-path]="${ARGS[$i]}" ;;
+
+			"--")
 				break ;;
 
-			--*)
-				>&2 echo "fopus: ${list_args["$i"]}: invalid option"
+			"--"*)
+				>&2 echo "fopus: ${ARGS["$i"]}: invalid option"
 				echo "Try 'fopus --help' for more information."
 				exit 1 ;;
 
 			*)
-				i=$((i-1))
+				((i--))
 				break ;;
 		esac
-		i=$((i+1))
+		((i++))
 	done
 
-	i=$((i+1))
-	while [[ $i -lt $N ]]; do
-		list_files+=( "${list_args["$i"]}" )
-		i=$((i+1))
-	done
-}
-
-filter_evaluate_files()
-{
-	local i=""
-	local file=""
-	local command_continue=""
-
-	if [[ "${fopus_config[one]}" == "true" ]]; then
-		command_continue=( exit 1 )
-	else
-		command_continue=( continue )
+	if [[ -z "${CONFIG[part-size]}" ]]; then
+		CONFIG[part-size]="1073741824"
 	fi
 
+	((i++))
+	FILES=("${ARGS[@]:$i}")
 
-	for i in "${!list_files[@]}"; do
-		cd "$origins_path" || exit 1
-		file="${list_files[$i]}"
+	return 0
+}
 
+evaluate_files()
+{
+	local file=""
+
+	declare -i i=0
+	declare -ir N="${#FILES[@]}"
+
+	while [[ "$i" -lt "$N" ]]; do
+		file="${FILES[$i]}"
 		if [[ ! -e "$file" ]]; then
 			>&2 echo "fopus: $file: No such file or directory"
-			"${command_continue[@]}"
+			exit 1
 		elif [[ ! -r "$file" ]]; then
 			>&2 echo "fopus: $file: Permission denied"
-			"${command_continue[@]}"
+			exit 1
 		fi
 
 		if [[ -d "$file" ]]; then
 			cd "$file" || exit 1
 			file=$(pwd -P)
-			cd ..
 		else
 			file="$(cd "$(dirname "$file")" && pwd -P)/$(basename "$file")"
 		fi
 
-		echo "fopus: $(du -sh "$file")"
-		list_clean+=("$file")
+		FILES["$i"]="$file"
+		echo "fopus: $(du -shL "${file}")"
+		((i++))
 	done
+
+	cd "$ORIGIN" || exit 1
 
 	return 0
 }
 
-fopus_backup_main()
+fopus_backup()
 {
-	local TARGET_FILE="$1"
-	local FILE_EXTENSION=""
-	local MIME_TYPE=""
-	local LIST_FILES=("$@")
+	local -r LIST_FILES=("$@")
 
-	local backup_name=""
-	local backup_name_hash=""
-	local archive_name=""
-	local bak_dir_parent=""
-	local bak_dir_child=""
+	local BACKUP_FILE=""
+	local BACKUP_PATH=""
+	local BACKUP_DIR=""
 
-	local i=""
-	local N=""
-	local perprefix=""
-	local hash_value=""
+	local tmp=""
 
-	backup_name=$(basename -- "$TARGET_FILE")
-	backup_name=${backup_name// /_}
+	REPO_NAME=$(basename -- "${LIST_FILES[0]}")
+	REPO_NAME=${REPO_NAME// /_}
 
-	if [[ -d "$TARGET_FILE" ]]; then
-		perprefix="dir"
-		archive_name="${perprefix}_${backup_name}.tar.xz"
+	BACKUP_FILE="${REPO_NAME}.tar.xz"
+
+	tmp=$(echo "${LIST_FILES[0]}" | "$sha1sum_tool")
+	tmp="$REPO_NAME-${tmp:0:11}"
+
+	if [[ "${CONFIG[group-by-name]}" = "true" ]]; then
+		BACKUP_PATH="$OUTPUT_PATH/$tmp"
+		BACKUP_DIR="backup_$DATE"
 	else
-		perprefix="file"
-		if [[ "$backup_name" =~ \.[:alnum:]+$ ]]; then
-			FILE_EXTENSION="${backup_name##*.}"
-		fi
-		MIME_TYPE=$(file --mime-type -b "$TARGET_FILE")
-		if [[ "$MIME_TYPE" == "application/x-xz" || "$FILE_EXTENSION" == "xz" ]]; then
-			archive_name="${perprefix}_${backup_name}"
-		elif [[ "$MIME_TYPE" == "application/x-tar" || "$FILE_EXTENSION" == "tar" ]]; then
-			archive_name="${perprefix}_${backup_name}.xz"
-		else
-			archive_name="${perprefix}_${backup_name}.tar.xz"
-		fi
+		BACKUP_PATH="$OUTPUT_PATH/backup_$DATE"
+		BACKUP_DIR="$tmp"
 	fi
-
-	hash_value=$(echo "$TARGET_FILE" | "$sha1sum_tool")
-	backup_name_hash="$backup_name-${hash_value:0:7}"
-
-	if [[ "${fopus_config[group-by-name]}" == "true" ]]; then
-		bak_dir_parent="$backup_name_hash"
-		bak_dir_child="bak_$DATE"
-	else
-		bak_dir_parent="bak_$DATE"
-		bak_dir_child="$backup_name_hash"
-	fi
-
 
 	cd "$HOME" || exit 1
 
-	if [[ -e "$root_path/$bak_dir_parent/$bak_dir_child" ]]; then
+	if [[ -e "$BACKUP_PATH/$BACKUP_DIR" ]]; then
 		>&2 echo "fopus: cannot create backup: Backup exists"
-		return 0
+		exit 1
 	fi
 
 	# show backup details
-	echo "Source $TARGET_FILE"
-	if [[ "${fopus_config[one]}" == "true" ]]; then
-		i=1
+	echo "Source ${LIST_FILES[0]}"
+	if [[ "${CONFIG[one]}" = "true" ]]; then
+		declare -i i=1
 		N="${#LIST_FILES[@]}"
 		while [[ $i -lt $N ]]; do
 			echo "       ${LIST_FILES[$i]}"
-			i=$((i+1))
+			((i++))
 		done
 	fi
-	echo "Backup $root_path/$bak_dir_parent/$bak_dir_child"
+	echo "Backup $BACKUP_PATH/$BACKUP_DIR"
 
-	du -sh "${LIST_FILES[@]}"
+	(du -shL "${LIST_FILES[@]}")
 
 	echo "fopus: start backup file"
-	if [[ "$DRY_RUN" = false ]]; then
-		mkdir -p "$root_path/$bak_dir_parent/$bak_dir_child" || exit 1
-		cd "$root_path/$bak_dir_parent/$bak_dir_child" || exit 1
+	if [[ "$DRY_RUN" = "false" ]]; then
+		mkdir -p "$BACKUP_PATH/$BACKUP_DIR" || exit 1
+		cd "$BACKUP_PATH/$BACKUP_DIR" || exit 1
 	fi
-
 
 	# compress
 	echo "fopus: archive and compress"
-	if [[ "$MIME_TYPE" == "application/x-xz" || "$FILE_EXTENSION" == "xz" ]]; then
-		echo "Skip."
-		if [[ "$DRY_RUN" = false ]]; then
-			cp "${LIST_FILES[@]}" "$archive_name"
-		fi
-	elif [[ "$MIME_TYPE" == "application/x-tar" || "$FILE_EXTENSION" == "tar" ]]; then
-		echo "Compress only."
-		if [[ "$DRY_RUN" = false ]]; then
-			cat "${LIST_FILES[@]}" | xz --threads=0 -z -vv -k - > "$archive_name"
-		fi
-	elif [[ "$DRY_RUN" = false ]]; then
-		tar -cvf - -- "${LIST_FILES[@]}" 2> "list_${perprefix}_${backup_name}" | xz --threads=0 -z -vv - > "$archive_name"
+	if [[ "$DRY_RUN" = "false" ]]; then
+		tar -cvf - -- "${LIST_FILES[@]}" 2> "${REPO_NAME}.txt" | xz --threads=0 -z -vv - > "$BACKUP_FILE"
 	fi
 
 	# encrypt
-	if [[ "$DRY_RUN" = false ]]; then
-		if ! age --encrypt --passphrase "$archive_name" > "$archive_name.age"; then
+	if [[ "$DRY_RUN" = "false" ]]; then
+		if ! age --encrypt --passphrase "$BACKUP_FILE" > "$BACKUP_FILE.age"; then
 			return 1
 		fi
 	fi
 
 	# split
 	echo "fopus: split"
-	if ! fopus_split_part "$archive_name"; then
+	if ! split_file "$BACKUP_FILE"; then
 		return 1
 	fi
 
 	# hash and file permission
-	"$DRY_RUN" || cd ..
-	if ! fopus_hash_permission_part "$bak_dir_child"; then
+	[[ "$DRY_RUN" = "true" ]] || cd ..
+	if ! hash_permission "$BACKUP_DIR"; then
 		return 1
 	fi
 
 	return 0
 }
 
-fopus_hash_permission_part()
+split_file()
 {
-	local bak_dir_child="$1"
+	if [[ "${CONFIG[part-size]}" = "-1" ]]; then
+		echo "Skip."
+		return 0
+	fi
 
+	local FILE_SIZE=""
+	local LIMIT_SIZE=""
+
+	if [[ "$DRY_RUN" = false ]]; then
+		FILE_SIZE=$(stat -c %s "$1.age")
+		LIMIT_SIZE=$(echo "${CONFIG[part-size]}" | numfmt --from=iec)
+
+		if [[ "$FILE_SIZE" -gt "$LIMIT_SIZE" ]]; then
+			# split
+			if ! split --verbose --bytes="${CONFIG[part-size]}" \
+				"$1.age" "$1.age_"; then
+					return 1
+			fi
+		fi
+	fi
+
+	return 0
+}
+
+hash_permission()
+{
 	# hashes
 	echo "fopus: hashes"
-	if [[ "$DRY_RUN" = false ]]; then
-		(find "$bak_dir_child/" -type f -exec "$sha1sum_tool" {} \; >> SHA1SUMS)
-		(find "$bak_dir_child/" -type f -exec md5sum {} \; >> MD5SUMS)
+	if [[ "$DRY_RUN" = "false" ]]; then
+		(find "$1/" -type f -exec "$sha1sum_tool" {} \; >> SHA1SUMS)
+		(find "$1/" -type f -exec md5sum {} \; >> MD5SUMS)
 	fi
 
 	# file permission
 	echo "fopus: file permission"
-	"$DRY_RUN" && return 0
-
-	if ! chmod 700 "$bak_dir_child/"; then
-		return 1
-	fi
-	(find "$bak_dir_child/" -type f -exec chmod 600 {} \;)
-	(find "$bak_dir_child/" -type d -exec chmod 700 {} \;)
-
-	return 0
-}
-
-fopus_split_part()
-{
-	local size_value=""
-	local max_size_value=""
-	local archive_name="$1"
-
-	max_size_value=${fopus_config[max-size]}
-	if [[ "$DRY_RUN" = false ]]; then
-		size_value=$(stat -c %s "$archive_name.age")
-	fi
-
-	if [[ "${fopus_config[max-size]}" != "-1" && \
-			"$size_value" -gt "$max_size_value" ]]; then
-		"$DRY_RUN" && return 0
-
-		if ! split --verbose -b "$max_size_value" \
-			"$archive_name.age" "$archive_name.age_"; then
-				return 1
+	if [[ "$DRY_RUN" = "false" ]]; then
+		if ! chmod 700 "$1/"; then
+			return 1
 		fi
-	else
-		echo "Not necessary."
+		(find "$1/" -type f -exec chmod 600 {} \;)
+		(find "$1/" -type d -exec chmod 700 {} \;)
 	fi
 
 	return 0
 }
-
 
 check_requirements
 
-user_option="$1"
-if [[ -z "$user_option" ]]; then
+if [[ -z "$1" ]]; then
 	>&2 echo "fopus: missing file operand"
 	echo "Try 'fopus --help' for more information."
 	exit 1
 fi
 
-case "$user_option" in
-	--help)
+case "$1" in
+	"--help")
 		show_help ;;
 
-	--version)
-		echo "v$version" ;;
+	"--version")
+		echo "v${VERSION}" ;;
 
-	--dir|--)
-		fopus_main "${@:2}" ;;
-
-	# --*)
-	# 	>&2 echo "fopus: invalid option"
-	# 	echo "Try 'fopus --help' for more information."
-	# 	exit 1 ;;
+	"--")
+		main "${@:2}" ;;
 
 	*)
-		fopus_main "${@:1}" ;;
+		main "${@:1}" ;;
 esac
 
 exit 0
